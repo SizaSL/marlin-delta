3fb626e7379b35a35a4e7a1418f6bc00ca8fc3f8
Newtonmin <newtonmin@gmail.com>
2017-04-17 11:00:59 +1200

    Add fixed port name already set by udev rules

--- Build_linux.sh
+++ Build_linux.sh
@@ -1,2 +1,2 @@
 mkdir platformio-build
-platformio ci --verbose --board megaatmega2560 --project-option="targets=upload" --project-option="upload_port=/dev/ttyUSB1" --build-dir=platformio-build --keep-build-dir Marlin
+platformio ci --verbose --board megaatmega2560 --project-option="targets=upload" --project-option="upload_port=/dev/Marlin" --build-dir=platformio-build --keep-build-dir Marlin
